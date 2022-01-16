c427800dd3aff15f70e3de1e1ce46ef08aca6c34
Zeptooo <github@macstudio.pro>
2022-01-16 21:55:57 +1100

    remove: typescript, as it does not match

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-export class Database {
-  constructor(authorization?: string, url?: string);
-  public request(options: {path: string, method: string, headers?: object}, data?: any);
-  public get(key: string, options?: { raw?: boolean }): Promise<unknown>;
-  public set(key: string, data: any): Promise<string>;
-  public delete(key: string): Promise<string>;
-  public list(key?: string): Promise<string[]>;
-}
